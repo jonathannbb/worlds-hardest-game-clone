3739723ac7fc0f4e644337f73667ea93d1e0dd28
jonathan <jonathanbotina11@gmail.com>
2026-07-18 19:07:30 -0500

    Se agrego el punto de llegada al primer nivel, y se mejoraron algunos comentarios y organizacion del codigo

--- level.py
+++ level.py
@@ -2,7 +2,8 @@ import pygame
 
 conf_game = {
     1:{
-        "zona_inicio" : (100, 100, 30, 30)
+        "zona_inicio" : (100, 100, 30, 30),
+        "meta_final" : (500, 500, 30, 30)
     }
 }
 
@@ -10,7 +11,9 @@ class Nivel:
     def __init__(self, numero_nivel):
         datos = conf_game[numero_nivel]
         self.inicio_rect = pygame.Rect(datos["zona_inicio"])
+        self.final_rect = pygame.Rect(datos["meta_final"])
     def draw(self, Screen):
         pygame.draw.rect(Screen, (0,255,0), self.inicio_rect)
+        pygame.draw.rect(Screen, (0,255,0), self.final_rect)
 
 
--- main.py
+++ main.py
@@ -2,27 +2,24 @@ import pygame
 from player import Jugador
 from level import Nivel
 
-# 1. Iniciar Pygame
+#Iniciar Pygame
 pygame.init()
 
 
-# 2. Configurar el tamaño de la ventana (ancho, alto en píxeles y color de pantalla)
+#Configurar el tamaño de la ventana y color
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 blanco = (255,255,255)
-player = Jugador(100,100)
+
+#posicion inicial del jugador, y que el objeto no sobrepase el limite de la ventana
+poscision_inicial_jugador = [Jugador(105,105), Jugador(125,125)]
+player = poscision_inicial_jugador[0]
 limitScreen = screen.get_rect()
 
-
+#objeto que instacia el nivel (escenario)
 obstacle = Nivel(1)
 
-
-
-
-# 3. Ponerle un título a la ventana
-pygame.display.set_caption("Mi Ventana Principal")
-
-# 4. Bucle principal para que la ventana no se cierre sola
+#Bucle principal para que la ventana no se cierre sola con los diversos elementos del juego
 jugando = True
 while jugando:
     for evento in pygame.event.get():
@@ -32,17 +29,15 @@ while jugando:
             if evento.key == pygame.K_ESCAPE:
                 jugando = False
     
-    
+    #Coordenadas del jugador, por ahora con el objetivo de saber donde esta ubicado el jugador
+    #y asi plantear los niveles con mayor facilidad
+    pygame.display.set_caption(f"Posición Jugador -> X: {player.rect.x} | Y: {player.rect.y}")
+
     player.Move()
     player.rect.clamp_ip(limitScreen)
     screen.fill(blanco)
     obstacle.draw(screen)
     player.Draw(screen)
-    
-
-
-
-    
     
     pygame.display.flip()
     clock.tick(60)
